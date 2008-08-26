b402f47919daec57d6cd6841d168d59da3a2f4e4
Henrik Friedrichsen <hrkfrd@gmail.com>
2008-08-26 02:24:08 +0200

    change version to 0.3.3

--- version.h
+++ version.h
@@ -4,8 +4,8 @@
 #define APPNAME		"hrktorrent"
 #define MAJOR		0
 #define MINOR		3
-#define REVISION	2
+#define REVISION	3
 #define TAG			0
-#define VERSION		"0.3.2"
+#define VERSION		"0.3.3"
 
 #endif
