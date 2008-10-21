7e9b3cc7be5b3344b28e9a457c1d7c2a7a52b3a9
hrkfrd <hrkfrd@gmail.com>
2008-10-21 17:54:52 +0200

    bump versions to 0.3.4

--- version.h
+++ version.h
@@ -4,8 +4,8 @@
 #define APPNAME		"hrktorrent"
 #define MAJOR		0
 #define MINOR		3
-#define REVISION	3
+#define REVISION	4
 #define TAG			0
-#define VERSION		"0.3.3"
+#define VERSION		"0.3.4"
 
 #endif
