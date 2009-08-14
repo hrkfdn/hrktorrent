bb0b3c4ba1f4fd5af3f469e9dc3b8f6ba4750803
Henrik Friedrichsen <henrik@moon.universe.se>
2009-08-14 02:21:22 +0200

    update version

--- version.h
+++ version.h
@@ -4,8 +4,8 @@
 #define APPNAME		"hrktorrent"
 #define MAJOR		0
 #define MINOR		3
-#define REVISION	4
+#define REVISION	5
 #define TAG			0
-#define VERSION		"0.3.4"
+#define VERSION		"0.3.5"
 
 #endif
