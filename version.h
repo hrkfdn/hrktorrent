9fd5ce9c8ba211fa7986b927f32ff5e5b1affa2e
Henrik Friedrichsen <hrkfdn@gmail.com>
2009-09-21 20:37:43 +0200

    update changelog, bump version

--- version.h
+++ version.h
@@ -4,8 +4,8 @@
 #define APPNAME		"hrktorrent"
 #define MAJOR		0
 #define MINOR		3
-#define REVISION	5
+#define REVISION	6
 #define TAG			0
-#define VERSION		"0.3.5"
+#define VERSION		"0.3.6"
 
 #endif
